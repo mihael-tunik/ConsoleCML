4e8d83ae448e566ea94c0c0282a06d18120ba7a4
mihael-tunik <mihael.8112@yahoo.com>
2016-07-06 20:01:05 +0300

    Add files via upload
    tests

---
+++ test/tests_correct.bat
@@ -0,0 +1,39 @@
+:: correct inputs
+
+ConsoleCML
+ConsoleCML help
+ConsoleCML butane.cml -v null
+ConsoleCML butane.cml -v -p null
+ConsoleCML butane.cml -v -p -f null
+
+ConsoleCML butane.cml -v C:\jml\jmol.jar
+ConsoleCML butane.cml -v -p C:\jml\jmol.jar
+ConsoleCML butane.cml -v -p -f C:\jml\jmol.jar
+ConsoleCML methanol.cml -v null
+ConsoleCML methanol.cml -p null
+
+ConsoleCML methanol.cml -f null
+ConsoleCML methanol.cml -v -p null
+ConsoleCML methanol.cml -p -v null
+ConsoleCML methanol.cml -v -f null
+ConsoleCML methanol.cml -f -v null
+
+ConsoleCML methanol.cml -p -f null
+ConsoleCML methanol.cml -f -p null
+ConsoleCML methanol.cml -v -p -f null
+ConsoleCML methanol.cml -v -f -p null
+ConsoleCML methanol.cml -p -f -v null
+
+ConsoleCML methanol.cml -p -v -f null
+ConsoleCML methanol.cml -f -v -p null
+ConsoleCML methanol.cml -f -p -v null
+ConsoleCML cyclodecane.cml -v null
+ConsoleCML cyclodecane.cml -f null
+
+ConsoleCML eicosane.cml -v null
+ConsoleCML eicosane.cml -f null
+ConsoleCML tetracosane.cml -v -f null
+ConsoleCML tetracosane.cml -v -p null
+ConsoleCML tetracosane.cml -v -p -f null
+
+:: 30 tests completed
---
+++ test/tests_incorrect.bat
@@ -0,0 +1,40 @@
+:: incorrect inputs
+
+ConsoleCML i0.cml -v null
+ConsoleCML i0.cml -p null
+ConsoleCML i0.cml -f null
+ConsoleCML i0.cml -v -p null
+ConsoleCML i0.cml -p -v null
+
+ConsoleCML i0.cml -f -v null
+ConsoleCML i0.cml -f -p null
+ConsoleCML i0.cml -v -f null
+ConsoleCML i0.cml -p -f null
+ConsoleCML i0.cml -p -f -v null
+
+ConsoleCML i0.cml -p -v -f null
+ConsoleCML i0.cml -v -p -f null
+ConsoleCML i0.cml -v -f -p null
+ConsoleCML i0.cml -f -p -v null
+ConsoleCML i0.cml -f -v -p null
+
+ConsoleCML i1.cml -v null
+ConsoleCML i1.cml -p null
+ConsoleCML i1.cml -f null
+ConsoleCML i1.cml -v -p null
+ConsoleCML i1.cml -p -v null
+
+ConsoleCML i1.cml -f -v null
+ConsoleCML i1.cml -f -p null
+ConsoleCML i1.cml -v -f null
+ConsoleCML i1.cml -p -f null
+ConsoleCML i1.cml -p -f -v null
+
+ConsoleCML i1.cml -p -v -f null
+ConsoleCML i1.cml -v -p -f null
+ConsoleCML i1.cml -v -f -p null
+ConsoleCML i1.cml -f -p -v null
+ConsoleCML i1.cml -f -v -p null
+
+:: 30 tests completed
+pause
